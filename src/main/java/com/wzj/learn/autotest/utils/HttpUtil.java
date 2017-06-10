182e038907de2f455f86d0d157e99384ab88a8fb
wangzhenjiang <wangzhenjiang@Linux.mshome.net>
2017-06-10 23:12:30 +0800

    提交代码

---
+++ src/main/java/com/wzj/learn/autotest/utils/HttpUtil.java
@@ -0,0 +1,150 @@
+package com.wzj.learn.autotest.utils;
+
+import org.apache.http.*;
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.routing.HttpRoute;
+import org.apache.http.conn.socket.LayeredConnectionSocketFactory;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.cookie.ClientCookie;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.impl.cookie.BasicClientCookie;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.ssl.SSLContexts;
+import org.apache.http.util.EntityUtils;
+
+import javax.net.ssl.SSLContext;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Http 工具类
+ * <p>
+ * Created by wangzhenjiang on 6/10/17.
+ */
+public class HttpUtil {
+
+    private static RequestConfig requestConfig;
+    private static PoolingHttpClientConnectionManager poolingHttpClientConnectionManager;
+
+    static {
+        poolingHttpClientConnectionManager = new PoolingHttpClientConnectionManager();
+        // Increase max total connection to 200
+        poolingHttpClientConnectionManager.setMaxTotal(200);
+        // Increase default max connection per route to 20
+        poolingHttpClientConnectionManager.setDefaultMaxPerRoute(20);
+        // Increase max connections for localhost to 50
+        poolingHttpClientConnectionManager.setMaxPerRoute(new HttpRoute(new HttpHost("localhost")), 50);
+
+        requestConfig = RequestConfig.custom()
+                .setSocketTimeout(2000)//
+                .setConnectTimeout(2000)
+                .setConnectionRequestTimeout(2000)
+                .setCookieSpec(CookieSpecs.DEFAULT)
+                .build();
+    }
+
+    public static String doGet(String url, CookieStore cookieStore) throws IOException {
+        return doGet(url, null, cookieStore);
+    }
+
+    public static String doGet(String url, Map<String, String> headers, CookieStore cookieStore) throws IOException {
+        HttpClient httpClient = getHttpClient();
+        HttpGet httpGet = new HttpGet(url);
+        if (headers != null && headers.size() > 0) {
+            headers.forEach((K, V) -> httpGet.setHeader(K, V));
+        }
+        HttpResponse httpResponse = httpClient.execute(httpGet);
+
+        System.out.println("Response Status: " + httpResponse.getStatusLine());
+//        Header[] resHeaders = httpResponse.getAllHeaders();
+        Header[] cookieHeaders = httpResponse.getHeaders("Set-Cookie");
+        System.out.println("Response Headers: " + Arrays.toString(cookieHeaders));
+        if (cookieHeaders != null && cookieHeaders.length > 0) {
+            Arrays.stream(cookieHeaders).forEach(header -> {
+                String targetCookieStr = header.getValue().split(";")[0];
+                String[] targetCookieArray = targetCookieStr.split("=", 2);
+                cookieStore.addCookie(new BasicClientCookie(targetCookieArray[0], targetCookieArray[1]));
+            });
+        }
+        return EntityUtils.toString(httpResponse.getEntity(), "utf-8");
+    }
+
+    public static String doPost(String url, Map<String, String> params, Map<String, String> headers) throws IOException {
+        return doPost(url, params, headers, null);
+    }
+
+    public static String doPost(String url, Map<String, String> params, Map<String, String> headers, CookieStore cookieStore) throws IOException {
+        HttpPost httpPost = new HttpPost(url);
+        if (params != null && params.size() > 0) {
+            List<NameValuePair> nameValuePairs = new ArrayList<>(params.size());
+            params.forEach((K, V) -> nameValuePairs.add(new BasicNameValuePair(K, V)));
+            UrlEncodedFormEntity urlEncodedFormEntity = new UrlEncodedFormEntity(nameValuePairs, "utf-8");
+//            urlEncodedFormEntity.setContentEncoding("utf-8");
+//            urlEncodedFormEntity.setContentType("application/x-www-form-urlencoded");
+            httpPost.setEntity(urlEncodedFormEntity);
+        }
+        if (headers != null && headers.size() > 0) {
+            headers.forEach((K, V) -> httpPost.setHeader(K, V));
+        }
+        HttpClient httpClient = url.startsWith("https")
+                ? cookieStore == null ? getHttpsClient() : getHttpsClient(cookieStore)
+                : cookieStore == null ? getHttpClient() : getHttpClient(cookieStore);
+//        httpPost.setConfig(requestConfig);
+//        httpClient.getParams().setParameter("http.useragent", "Custom Browser");
+//        httpClient.getParams().setParameter(CoreProtocolPNames.PROTOCOL_VERSION, HttpVersion.HTTP_1_1);
+
+        HttpResponse httpResponse = httpClient.execute(httpPost);
+        System.out.println("Response Status: " + httpResponse.getStatusLine());
+        Header[] resHeaders = httpResponse.getAllHeaders();
+        System.out.println("Response Headers: " + Arrays.toString(resHeaders));
+        String response = EntityUtils.toString(httpResponse.getEntity(), "utf-8");
+        EntityUtils.consume(httpResponse.getEntity());
+        return response;
+    }
+
+    public static CloseableHttpClient getHttpClient() {
+        return HttpClients.custom()
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+    }
+
+    public static CloseableHttpClient getHttpClient(CookieStore cookieStore) {
+        return HttpClients.custom()
+                .setDefaultRequestConfig(requestConfig)
+                .setDefaultCookieStore(cookieStore)
+                .build();
+    }
+
+    public static CloseableHttpClient getHttpsClient() {
+        return HttpClients.custom()
+                .setSSLSocketFactory(createSSLSocketFactory())
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+    }
+
+    public static CloseableHttpClient getHttpsClient(CookieStore cookieStor) {
+        return HttpClients.custom()
+                .setSSLSocketFactory(createSSLSocketFactory())
+                .setDefaultRequestConfig(requestConfig)
+                .setDefaultCookieStore(cookieStor)
+                .build();
+    }
+
+    private static LayeredConnectionSocketFactory createSSLSocketFactory() {
+        SSLContext sslContext = SSLContexts.createSystemDefault();
+        return new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
+    }
+}
---
+++ src/test/java/com/wzj/learn/autotest/testcase/TestLogin.java
@@ -0,0 +1,66 @@
+package com.wzj.learn.autotest.testcase;
+
+import com.gargoylesoftware.htmlunit.util.Cookie;
+import com.wzj.learn.autotest.utils.HttpUtil;
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.CookieStore;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by wangzhenjiang on 6/10/17.
+ */
+public class TestLogin {
+
+    @Test
+    public void testLoginJD() throws IOException {
+
+        String url = "https://passport.jd.com/uc/loginService?uuid=ff75c8a6-9692-421a-8e84-738fded1a622&ReturnUrl=https%3A%2F%2Fwww.jd.com%2F&r=0.26373833994048623&version=2015";
+
+        Map<String,String> headers = new HashMap<>();
+        headers.put("Accept","text/plain, */*; q=0.01");
+//        headers.put("Origin","https://passport.jd.com");
+        headers.put("Connection","keep-alive");
+//        headers.put("Content-Length","1665");
+//        headers.put("X-Requested-With","XMLHttpRequest");
+        headers.put("Content-Type","application/x-www-form-urlencoded; charset=UTF-8");
+        headers.put("User-Agent","Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36");
+
+        Map<String,String> params = new HashMap<>();
+        params.put("uuid","ff75c8a6-9692-421a-8e84-738fded1a622");
+        params.put("eid","WTDOFRR7ZGT5OSUSQY33K674BAHUCHGRF2DW62KQ6ZYT54V7FIRFFHR2HSR5UPJXHY7QYDF2POLL6CFBCJYA5QHZIA");
+        params.put("fp","3ec217138fb4b378774f1d9db38e899e");
+        params.put("_t","_nteHNir");
+        params.put("loginType","f");
+        params.put("loginname","18682095432");
+        params.put("nloginpwd","IzTIKZ04OyQW9y/J+9C5YZGK1mQEb6S+lxpQoBvVmsWx82KBpJtd2s7DWmp1SrfreVQ7PQEUZKHWrWQ+lh5BrLBs3eiQEzdB+zj6fwQKTiZ0C8JIIhzGsWjVR8uNtOXqc8ddrpGJrB9bnjuwgjPV+ZQvHSAUX0waL2Mk1JxnBdk=");
+        params.put("pubKey","MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDC7kw8r6tq43pwApYvkJ5laljaN9BZb21TAIfT");
+        params.put("sa_token","B68C442BE645754F33277E701208059080DD726A94A73F76DEC3053A838549C06EB7D3797CE1C5BBE7C2B2EF9CA7D467C820D6D2743D5D610B7AEC435D4160C8D69FD80591E9CF58440F6D8C144D610994FFCFF0A42DBD7FAEDE5D7FECED53B6A762C3D9E3C78BD6B494F0FBE0AFE52C4F8DB2BC43BC9D3304EEFEB1BF355FC2AB4C0EAA98940CBFA40A312A0D28910D53C8B002D379997E8E82676E2B3FCC9EB2471EBC715B38E4433F782BAC66F9467FA3EF88925B9E7028A17DE806DD14E38D5A865C8C5FA7F1E05BEAF332A03F1E7828230F939A481ED6709646D4EDF6213B3897F5FE3FFA00C297E63D05FA54AB497788A754CB07E32046103680D42B41CDFEF14019E1636DF60C3EED17E88CE58AA1ADAFEFB6840441ED82BF646085615F730553BAC29472D132599CBBB31712913446E7BB40F38373AFEE1F2D6A1FFD8F2D68982F0C35227A16F6D164C61B9A98F15DFA68D62B4B7B78C06DEF32FD01098ED23C282D12049369300EB380017F6A0B47D781E5B6BE87421E506BBDE14D918292D714DB50B12F6FD5D6647F146BB6D06909EF7AF2FE7D72C067F139DDEA5B1EBC28ACB341BCC90F99D49B112C3A3D7FF28205539A5B5FF2C280D878B7419D78FDF69DB1DCF9DFAC7CDED8422D3A4F1DEC0E74C7AB7AB91ABB16536736AA");
+        params.put("seqSid","3259819117999454001");
+
+        String response = HttpUtil.doPost(url,params,headers);
+        System.out.println(response);
+
+    }
+
+    @Test
+    public void testRes() throws IOException {
+        String url = "https://passport.jd.com/new/login.aspx?ReturnUrl=https%3A%2F%2Fwww.jd.com%2F";
+        CookieStore cookieStore = new BasicCookieStore();
+        String response = HttpUtil.doGet(url,cookieStore);
+//        System.out.println(response);
+
+    }
+
+    @Test
+    public void testSplit(){
+        String a = "aete=sdfasdf=sdf2=";
+        System.out.println(Arrays.toString(a.split("=",2)));
+    }
+}
