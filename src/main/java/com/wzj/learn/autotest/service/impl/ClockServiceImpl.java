f57204ac899fa344306b676c5835743e7f20c6d5
wangzhenjiang <wangzhenjiang@jd.com>
2017-11-09 20:20:42 +0800

    '修改打卡时间'

--- src/main/java/com/wzj/learn/autotest/service/impl/ClockServiceImpl.java
+++ src/main/java/com/wzj/learn/autotest/service/impl/ClockServiceImpl.java
@@ -5,8 +5,6 @@ import com.wzj.learn.autotest.service.DriverService;
 import com.wzj.learn.autotest.utils.PropertiesUtil;
 import org.apache.commons.lang3.RandomUtils;
 import org.openqa.selenium.*;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -27,17 +25,13 @@ public class ClockServiceImpl implements ClockService {
         String url = PropertiesUtil.get("erp.url");
         String username = PropertiesUtil.get("erp.username");
         String password = PropertiesUtil.get("erp.password");
-        //打开Chrome，并跳转至ERP页面
-        System.out.println("------打开浏览器----------");
         webDriver.get(url);
-        System.out.println("------登录ERP----------");
         //登录ERP
         loginERP(username, password);
     }
 
 //    @PreDestroy
     public void quitWebDriver() {
-        System.out.println("------退出浏览器----------");
         webDriver.quit();
     }
 
@@ -52,36 +46,29 @@ public class ClockServiceImpl implements ClockService {
     }
 
     private void closeTips() {
-        System.out.println("----处理弹窗-----");
-        try {
-            //等待弹窗出现
-            Thread.sleep(2500);
-        } catch (InterruptedException e) {
-        }
         //处理弹窗
         WebElement spanElement = findElement(By.className("layui-layer-setwin"));
-        if(spanElement != null){
+        if (spanElement != null) {
             WebElement aElement = spanElement.findElement(By.tagName("a"));
             //执行javascript
             JavascriptExecutor executor = (JavascriptExecutor) webDriver;
             executor.executeScript("arguments[0].click()", aElement);
-            System.out.println("----处理弹窗完成-----");
         }
     }
 
     private void doClockIn() {
-//        try {
-//            Thread.sleep(RandomUtils.nextInt(600000, 1200000));
-//        } catch (InterruptedException e) {//ignore
-//        }
-        System.out.println("-------开始打卡--------");
+        try {
+            //随机休眠 1~19 分钟
+            Thread.sleep(RandomUtils.nextInt(60000, 1140000));
+//            Thread.sleep(RandomUtils.nextInt(1000, 2000));
+        } catch (InterruptedException e) {//ignore
+        }
         //嘿嘿
         WebElement divElement = findElement(By.id("clockLink"));
         WebElement buttonElement = divElement.findElement(By.tagName("button"));
         //执行javascript
         JavascriptExecutor executor = (JavascriptExecutor) webDriver;
         executor.executeScript("arguments[0].click()", buttonElement);
-        System.out.println("-------打卡结束--------");
     }
 
     @Override
@@ -95,10 +82,15 @@ public class ClockServiceImpl implements ClockService {
         element = findElement(By.id("password"));
         element.sendKeys(erpPwd);
         element = findElement(By.className("formsubmit_btn"));
+        try {
+            //自动输入太快，休眠2秒后提交
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+        }
         element.submit();
     }
 
-    protected WebElement findElement(By by) {
+    private WebElement findElement(By by) {
         try {
             return webDriver.findElement(by);
         } catch (NoSuchElementException e) {
